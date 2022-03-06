400eab4a358020e085b4044c8868928278ff1287
Schwanz Dias <s.schwanz.dias@tue.nl>
2022-03-06 14:46:58 +0100

    Improving sparse codec demo

--- wnnsimulink/msfcn_sparse_codec.m
+++ wnnsimulink/msfcn_sparse_codec.m
@@ -84,46 +84,46 @@ function setup(block)
     block.RegBlockMethod('CheckParameters', @CheckPrms);
     block.RegBlockMethod('Start', @Start);
     block.RegBlockMethod('Outputs', @Outputs);
-    block.RegBlockMethod('Update', @Update);
+%     block.RegBlockMethod('Update', @Update);
     block.RegBlockMethod('Terminate', @Terminate);
 
 %endfunction
 
 function DoPostPropSetup(block)
     % Get parameters
-    dense_vector_length = block.DialogPrm(2).Data;
-    sparse_vector_length = block.DialogPrm(3).Data;
-    codec_type = block.DialogPrm(11).Data;
+%     dense_vector_length = block.DialogPrm(2).Data;
+%     sparse_vector_length = block.DialogPrm(3).Data;
+%     codec_type = block.DialogPrm(11).Data;
 
      % Set the input/output dimensions and data types properly
-    switch codec_type
-        case 0 % encoder
-            output_name = 'sparse_vector_output';
-            output_dims = sparse_vector_length;
-            output_dtype = 3; % uint8
-        case 1 % decoder
-            output_name = 'dense_vector_output';
-            output_dims = dense_vector_length;
-            output_dtype = 0;  % double;
-        otherwise
-            output_dims = [];
-            output_dtype = [];
-    end
+%     switch codec_type
+%         case 0 % encoder
+%             output_name = 'sparse_vector_output';
+%             output_dims = sparse_vector_length;
+%             output_dtype = 3; % uint8
+%         case 1 % decoder
+%             output_name = 'dense_vector_output';
+%             output_dims = dense_vector_length;
+%             output_dtype = 0;  % double;
+%         otherwise
+%             output_dims = [];
+%             output_dtype = [];
+%     end
 
     %% Setup Dwork
-    block.NumDworks = 2;
+    block.NumDworks = 1;
     
-    block.Dwork(1).Name            = output_name;   
-    block.Dwork(1).Dimensions      = output_dims;
-    block.Dwork(1).DatatypeID      = output_dtype;
+%     block.Dwork(1).Name            = output_name;   
+%     block.Dwork(1).Dimensions      = output_dims;
+%     block.Dwork(1).DatatypeID      = output_dtype;
+%     block.Dwork(1).Complexity      = 'Real';
+%     block.Dwork(1).UsedAsDiscState = true;
+
+    block.Dwork(1).Name            = 'codec_ownership';   
+    block.Dwork(1).Dimensions      = 1;
+    block.Dwork(1).DatatypeID      = 8;  % boolean
     block.Dwork(1).Complexity      = 'Real';
     block.Dwork(1).UsedAsDiscState = true;
-
-    block.Dwork(2).Name            = 'codec_ownership';   
-    block.Dwork(2).Dimensions      = 1;
-    block.Dwork(2).DatatypeID      = 8;  % boolean
-    block.Dwork(2).Complexity      = 'Real';
-    block.Dwork(2).UsedAsDiscState = true;
 
 %endfunction
 
@@ -188,53 +188,98 @@ function CheckPrms(block)
 function Start(block)
     % Get parameters
     codec_id = block.DialogPrm(1).Data;
-    dense_vector_length = block.DialogPrm(2).Data;
-    sparse_vector_length = block.DialogPrm(3).Data;
+%     dense_vector_length = block.DialogPrm(2).Data;
+%     sparse_vector_length = block.DialogPrm(3).Data;
     max_depth = block.DialogPrm(4).Data;
     learning_rate = block.DialogPrm(5).Data;
     min_splitting_volume = block.DialogPrm(6).Data;
     min_bounds = block.DialogPrm(7).Data;
     max_bounds = block.DialogPrm(8).Data;
     sparse_vectors_file = block.DialogPrm(9).Data;
-    codec_type = block.DialogPrm(11).Data;
+%     codec_type = block.DialogPrm(11).Data;
 
     % Create the sparse codec
     codec_ownership = py.wnnlib.sparse_codec.create(codec_id, max_depth, learning_rate, min_splitting_volume, min_bounds, max_bounds, sparse_vectors_file);
 
     % Default output values to zero
-    switch codec_type
-        case 0 % encoder
-            output_vector = zeros(1, sparse_vector_length, 'uint8');
-        case 1 % decoder
-            output_vector = zeros(1, dense_vector_length, 'double');
-        otherwise
-            output_vector = [];
-    end
+%     switch codec_type
+%         case 0 % encoder
+%             output_vector = zeros(1, sparse_vector_length, 'uint8');
+%         case 1 % decoder
+%             output_vector = zeros(1, dense_vector_length, 'double');
+%         otherwise
+%             output_vector = [];
+%     end
 
     % Store output values
-    block.Dwork(1).Data = output_vector(:);
-    block.Dwork(2).Data = logical(codec_ownership);
+%     block.Dwork(1).Data = output_vector(:);
+%     block.Dwork(2).Data = logical(codec_ownership);
+      block.Dwork(1).Data = logical(codec_ownership);
 
 %endfunction
+
+% function Outputs(block)
+%     % Get parameters
+%     codec_id = block.DialogPrm(1).Data;
+%     debug_flag = block.DialogPrm(10).Data;
+% 
+%     % Restore the last output values
+%     block.OutputPort(1).Data = block.Dwork(1).Data;
+% 
+%     if(debug_flag)
+%         % Debug sparse codec
+%         py.wnnlib.sparse_codec.debug(codec_id, 0);
+%     end
+  
+%endfunction
+
+% function Update(block)
+%     % Get parameters
+%     codec_id = block.DialogPrm(1).Data;
+%     codec_type = block.DialogPrm(11).Data;
+% 
+%     % Update the output properly
+%     switch codec_type
+%         case 0 % encoder
+%             % Get the dense vector
+%             input_dense_vector = block.InputPort(1).Data;
+% 
+%             disp("encoding:")
+%             disp(block.CurrentTime)
+%             disp(input_dense_vector)
+%             
+%             % Encode the given dense vector into a sparse vector
+%             output_sparse_vector = uint8(py.wnnlib.sparse_codec.encode(codec_id, input_dense_vector));
+% 
+%             disp(sum(output_sparse_vector))
+%             
+%             % Save the last output values
+%             block.Dwork(1).Data = output_sparse_vector(:);
+%         case 1 % decoder
+%             % Get the sparse vector
+%             input_sparse_vector = block.InputPort(1).Data;
+% 
+%             disp("decoding:");
+%             disp(block.CurrentTime)
+%             disp(sum(input_sparse_vector));
+%             
+%             % Learn the given input pattern
+%             output_dense_vector = double(py.wnnlib.sparse_codec.decode(codec_id, input_sparse_vector));
+% 
+%             disp(output_dense_vector);
+%             
+%             % Save the last output values
+%             block.Dwork(1).Data = output_dense_vector(:);
+%         otherwise
+%             block.Dwork(2).Data = [];
+%     end
+%   
+% %endfunction
 
 function Outputs(block)
     % Get parameters
     codec_id = block.DialogPrm(1).Data;
     debug_flag = block.DialogPrm(10).Data;
-
-    % Restore the last output values
-    block.OutputPort(1).Data = block.Dwork(1).Data;
-
-    if(debug_flag)
-        % Debug sparse codec
-        py.wnnlib.sparse_codec.debug(codec_id, 0);
-    end
-  
-%endfunction
-
-function Update(block)
-    % Get parameters
-    codec_id = block.DialogPrm(1).Data;
     codec_type = block.DialogPrm(11).Data;
 
     % Update the output properly
@@ -242,23 +287,40 @@ function Update(block)
         case 0 % encoder
             % Get the dense vector
             input_dense_vector = block.InputPort(1).Data;
+
+%             disp("encoding:")
+%             disp(block.CurrentTime)
+%             disp(input_dense_vector)
             
             % Encode the given dense vector into a sparse vector
             output_sparse_vector = uint8(py.wnnlib.sparse_codec.encode(codec_id, input_dense_vector));
+
+%             disp(sum(output_sparse_vector))
             
-            % Save the last output values
-            block.Dwork(1).Data = output_sparse_vector(:);
+            % Set the output values
+            block.OutputPort(1).Data = output_sparse_vector(:);
         case 1 % decoder
             % Get the sparse vector
             input_sparse_vector = block.InputPort(1).Data;
+
+%             disp("decoding:");
+%             disp(block.CurrentTime)
+%             disp(sum(input_sparse_vector));
             
             % Learn the given input pattern
             output_dense_vector = double(py.wnnlib.sparse_codec.decode(codec_id, input_sparse_vector));
+
+%             disp(output_dense_vector);
             
-            % Save the last output values
-            block.Dwork(1).Data = output_dense_vector(:);
+            % Set the output values
+            block.OutputPort(1).Data = output_dense_vector(:);
         otherwise
-            block.Dwork(2).Data = [];
+            block.OutputPort(1).Data = [];
+    end
+
+    if(debug_flag)
+        % Debug sparse codec
+        py.wnnlib.sparse_codec.debug(codec_id, 0);
     end
   
 %endfunction
@@ -268,7 +330,7 @@ function Terminate(block)
     codec_id = block.DialogPrm(1).Data;
 
     % Get codec ownership
-    codec_ownership = block.Dwork(2).Data;
+    codec_ownership = block.Dwork(1).Data;
     
     % Delete sparse codec
     if codec_ownership
